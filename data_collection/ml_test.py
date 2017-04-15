92218ac37d3e375b21734948819b8ef917f4d808
Amit Mizrahi <amit@amizrahi.com>
2017-04-15 17:38:49 -0400

    Implement R^2 calculation

--- data_collection/ml_test.py
+++ data_collection/ml_test.py
@@ -1,6 +1,7 @@
 from processing import JsonLoader
 from predictive_models import TimeModel
 from matplotlib import pyplot as plt
+from sklearn.metrics import r2_score
 
 def test_time_model():
     """ Manual test of time ML model. Plots predictions of attendance vs. time. """
@@ -8,9 +9,17 @@ def test_time_model():
     data = j.time_model_data()
     t = TimeModel()
     t.train(data)
+
+    print "R^2:"
+    preds = []
+    for time in data[:,0]:
+        preds.append(t.test(time))
+    print r2_score(data[:,1], preds)
+
     predictions = []
     for i in xrange(0, 24*4):
         predictions.append(t.test(i/4.0))
+
 
     fig = plt.figure()
     plt.plot([i/4.0 for i in xrange(0, 24*4)], predictions)
