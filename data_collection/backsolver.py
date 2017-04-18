8a4be70e2b0b7c2e8753333d8f8c2523bdb22d98
Amit Mizrahi <amit@amizrahi.com>
2017-04-18 10:24:10 -0400

    Move to machine_learning directory

--- data_collection/backsolver.py
+++
@@ -1,6 +0,0 @@
-from predictive_models import EventModel
-
-class Backsolver(object):
-    """ Uses EventModel to, when given a textual description, suggest
-    optimal time-location pairs. """
-    pass
