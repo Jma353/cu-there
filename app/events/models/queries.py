0e3f9a328ec98549f51ec6fcacb49737d7bea656
Jma353 <jma353@cornell.edu>
2017-04-18 03:49:58 -0400

    Refactor ML to use db format

---
+++ app/events/models/queries.py
@@ -0,0 +1,10 @@
+# Several functions for building & executing queries on the data
+# modeled in this module
+
+def get_events(event_ids):
+  """
+  Builds an event id query and return a list of events
+  with said id's
+  """
+  pass
+  
