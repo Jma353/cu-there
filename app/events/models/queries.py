60fa8b11754955702065fcec05bcb4b722be1601
Jma353 <jma353@cornell.edu>
2017-04-18 20:35:37 -0400

    Stub endpoints

--- app/events/models/queries.py
+++ app/events/models/queries.py
@@ -2,14 +2,20 @@
 # modeled in this module
 
 from event import Event
+from venue import Venue
 from sqlalchemy.orm import joinedload
 
 def get_events(event_ids):
   """
-  Builds an event id query and return a list of events
-  with said id's
+  Returns a list of events of said id's
   """
   return [
     Event.query.options(joinedload('venue')).get(i)
     for i in event_ids
   ]
+
+def get_venues(venue_ids):
+  """
+  Returns a list of venues of said id's
+  """
+  return [Venue.query.get(i) for i in venue_ids]
