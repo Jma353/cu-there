ce64be1b279665ca4ec699c8b1e735102751c94b
Jma353 <jma353@cornell.edu>
2017-04-18 21:51:31 -0400

    Daniel take the wheel

--- app/events/controllers/venues_controller.py
+++ app/events/controllers/venues_controller.py
@@ -8,23 +8,11 @@ def get_venues():
   """
   Grab a sample list of venues
   """
-  # Read file
-  results = []
-  with open('./1491102320.json') as f:
-    results.extend(json.load(f))
-
-  # Get unique venues
-  venues = []; ids = set()
-  for r in results:
-    venue = r['venue']
-    if venue['id'] not in ids:
-      ids.add(venue['id'])
-      venues.append(venue)
-
+  
   result = {
     'success': True,
     'data': {
-      'venues': venues
+      'venues': []
     }
   }
 
