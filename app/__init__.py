04fcebdfc5b21d19c203b3bed08e780a9aeb9d12
Jma353 <jma353@cornell.edu>
2017-04-18 21:16:06 -0400

    Save me Annie

--- app/__init__.py
+++ app/__init__.py
@@ -1,32 +1,43 @@
 # Imports
 import os
+import json
 from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Configure app
 app = Flask(__name__, static_url_path='/static')
-app.config.from_object(os.environ["APP_SETTINGS"])
+app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+################
+# DATA STORAGE #
+################
 
 # DB
 db = SQLAlchemy(app)
 
+import store
+# Store everything
+store.store_venues('events.json', db)
+
 # Load events
 events = {}
 
-with open("events.json") as events_json:
+with open('events.json') as events_json:
   events_dict = json.load(events_json)
 
   # List of event dicts containing id, name, description, category
-  events = [{"id": event["id"], "name": event["name"],
-                  "description": event["description"] if event["description"] else "",
-                  "category": event["category"] if event["category"] else ""}
+  events = [{'id': event['id'], 'name': event['name'],
+                  'description': event['description'] if event['description'] else '',
+                  'category': event['category'] if event['category'] else ''}
                    for event in events_dict]
 
 # Load in TF-IDF matrix (or compute if it's first time)
 tfidf_vec = TfidfVectorizer(min_df=5, max_df=0.95, max_features=5000, stop_words='english')
 doc_by_term = tfidf_vec.fit_transform(event_descs).toarray()
+
+
 
 # Import + Register Blueprints
 from app.events import events as events
@@ -43,4 +54,4 @@ def any_root_path(path):
 # HTTP error handling
 @app.errorhandler(404)
 def not_found(error):
-  return render_template("404.html"), 404
+  return render_template('404.html'), 404
