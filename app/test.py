508aeb1c709b77d4eec6fbd774ef917ae22ae4d2
Annie Cheng <ac962@cornell.edu>
2017-04-23 20:04:54 -0400

    Add code to make coocurrence matrix

--- app/test.py
+++ app/test.py
@@ -5,7 +5,40 @@ from collections import defaultdict
 from sklearn.feature_extraction.text import TfidfVectorizer
 from ir_engine import IREngine
 
+def print_top_sim_words(matrix, idx_to_term, top_k=50):
+  """
+  Print top_k most and least similar word pairs given coocurrence matrix
+  """
+  # List format: [("c1 c2", cos_sim)...]
+  sims = []
+
+  # Build categ_sims list
+  for i in range(len(matrix)):
+    for j in range(i+1, len(matrix)):
+      c1, c2 = idx_to_term[i], idx_to_term[j]
+      cos_sim = matrix[i][j]
+
+      sims.append((c1 + " " + c2, cos_sim))
+
+  # Sort sims list by cosine similarity
+  score_list = sorted(sims, key=lambda x: -x[1])
+
+  # Print top k most similar pairs of words
+  print("MOST SIMILAR:")
+  for (words, score) in score_list[:top_k]:
+    print("[%.2f] %s" % (score, words))
+
+  print
+
+  # Print top k most dissimilar pairs of words
+  print("LEAST SIMILAR:")
+  for (words, score) in score_list[-top_k:][::-1]:
+    print("[%.2f] %s" % (score, words))
+
 def init_ir_engine():
+  """
+  Initialize IREngine with the query and data structures
+  """
   ### Get command-line arguments ###
 
   args = sys.argv
@@ -16,7 +49,7 @@ def init_ir_engine():
 
   query, query_categs = args[1], args[2].split(",")
 
-  events = {}
+  events = []
 
   with open("../events.json") as events_json:
     events_dict = json.load(events_json)
@@ -32,6 +65,21 @@ def init_ir_engine():
   tfidf_vec = TfidfVectorizer(min_df=5, max_df=0.95, max_features=5000, stop_words='english')
   event_descs = [event["description"] for event in events]
   doc_by_term = tfidf_vec.fit_transform(event_descs).toarray()
+
+  tfidf_vec = TfidfVectorizer(min_df=5, max_df=0.95, max_features=5000, stop_words='english')
+  doc_by_term = tfidf_vec.fit_transform(event_descs).toarray()
+
+  ### Create co-occurence matrix ###
+
+  bin_doc_by_term = doc_by_term.copy()
+  bin_doc_by_term[bin_doc_by_term > 0] = 1
+  cooccurence_matrix = np.dot(bin_doc_by_term.T, bin_doc_by_term)
+  np.fill_diagonal(cooccurence_matrix, 0)
+
+  # Testing: print out most and least similar word pairs
+  # term_to_idx = {v:i for i, v in enumerate(tfidf_vec.get_feature_names())}
+  # idx_to_term = {v:k for k, v in term_to_idx.items()}
+  # print_top_sim_words(cooccurence_matrix, idx_to_term)
 
   ### Create category-term matrix ###
 
