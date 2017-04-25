7801dabf6c646749dbed8452f74e419d59c14607
Jma353 <jma353@cornell.edu>
2017-04-25 19:45:08 -0400

    Minor config changes

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ import app.store as s
 if __name__ == '__main__':
   s.store_venues('events.json', db)
   print 'App live at http://0.0.0.0:5000'
-  socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+  socketio.run(app, host='0.0.0.0', port=5000)
