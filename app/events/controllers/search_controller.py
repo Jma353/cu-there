37dfc8416fdc5414cba311097d258f305bb51692
Jma353 <jma353@cornell.edu>
2017-04-30 22:32:06 -0400

    Some front-end and fix merge conflict problem

--- app/events/controllers/search_controller.py
+++ app/events/controllers/search_controller.py
@@ -67,7 +67,6 @@ def process_recs(es, sim_words, sim_categs, recs):
     r = recs['venues'][i]
     v = _venue_by_id(r['id'])
     v['events'] = [_event_by_id(e_id).name for e_id in r['events']]
-    v['suggested_time'] = recs['times'][i]['peak']
 
   graphs = []
 
--- front/app/components/search/RelatedWordsBar.js
+++ front/app/components/search/RelatedWordsBar.js
@@ -1,2 +1,69 @@
 import React from 'react';
-import axios from 'axios';
+import RelatedWordsCell from './RelatedWordsCell';
+require('../../../public/sass/RelatedWordsBar.scss');
+
+/**
+ * Related words
+ */
+class RelatedWordsBar extends React.Component {
+
+  /**
+   * Constructor
+   */
+  constructor (props) {
+    super(props);
+    this.state = {
+      used: [],
+      unused: []
+    };
+    this.handleUseWord = this.handleUseWord.bind(this);
+    this.handleRemoveWord = this.handleRemoveWord.bind(this);
+  }
+
+  /**
+   * Handle use a word
+   */
+  handleUseWord (i) {
+    let used = this.state.used.slice();
+    let unused = this.state.unused.slice();
+    used.push(unused.splice(i, 1));
+    this.setState({
+      used: used,
+      unused: unused
+    });
+  }
+
+  /**
+   * Handle remove a word from being used
+   */
+  handleRemoveWord (i) {
+    let used = this.state.used.slice();
+    let unused = this.state.unused.slice();
+    unused.push(used.splice(i, 1));
+    this.setState({
+      used: used,
+      unused: unused
+    });
+  }
+
+  generateCell (info, i) {
+    const key = (info.used ? 'used' : 'unused') + i;
+    return <RelatedWordsCell
+      key={key}
+      used={info.used}
+      word={info.word}
+      handleUse={this.handleUseWord}
+      handleRemove={this.handleRemoveWord}
+      id={i} />;
+  }
+
+  /**
+   * Render
+   */
+  render () {
+
+
+  }
+}
+
+export default RelatedWordsBar;
