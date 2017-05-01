86bbf534de64fb1ae8cf3062d6a250f21c317d93
Daniel Li <dl743@cornell.edu>
2017-04-30 23:38:32 -0400

    Remove fancy font ): and fix related events

--- app/events/controllers/search_controller.py
+++ app/events/controllers/search_controller.py
@@ -68,7 +68,7 @@ def process_recs(es, sim_words, sim_categs, recs):
   for i in xrange(len(recs['venues'])):
     r = recs['venues'][i]
     v = _venue_by_id(r['id'])
-    v['events'] = [_event_by_id(e_id).name for e_id in r['events'] if _event_by_id(e_id) is not None]
+    v['events'] = r['events']
 
   graphs = []
 
